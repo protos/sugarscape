6cdabb696ee470386f693dfd08fe02d9c17b798b
Colm De Barra <cdebarra@marktplaats.nl>
2018-12-12 17:26:42 +0100

    Bootstrapping app

---
+++ app/src/Main.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './Main.css';
+
+class Main extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          SugarScape!!!
+        </header>
+        <div className="main-content">
+          
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Main;
